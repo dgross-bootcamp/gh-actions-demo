5877d7d9f9f22bd01a21628f425ccd91e2c7098f
Daniel Gross <dgross@instructors.2u.com>
2022-04-30 11:07:27 -0400

    attempt to fail deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = "App";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to our GitHub Actions Demo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
